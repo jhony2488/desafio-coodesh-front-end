c23d753a9136a41808d0dedf2ea9d1702882cfce
jhony2488 <jhon.araujo2488@gmail.com>
2021-12-01 17:13:30 -0300

    .

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,7 +17,7 @@ const Home: NextPage = () => {
       
       <main>
         <h1>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js</a>
         </h1>
 
         <p >
