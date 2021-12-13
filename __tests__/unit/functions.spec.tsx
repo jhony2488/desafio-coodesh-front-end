d4dc7baaa92689a06a03566f9fbbe551edc89f4a
jhony2488 <jhon.araujo2488@gmail.com>
2021-12-13 09:41:16 -0300

    test: units tests created

--- __tests__/unit/functions.spec.tsx
+++ __tests__/unit/functions.spec.tsx
@@ -1,11 +1,25 @@
-import { dateFormat, parOrImpar } from '../../src/utils/functions';
+import { dateFormat, parOrImpar, sortFunction } from '../../src/utils/functions';
+import { mockInputSearch, mockupSortNovas, mockupSortAntigas } from '../../src/utils/input'
 
 describe('Functions', () => {
   it('should format date value', () => {
     expect(dateFormat('2021-11-30T22:18:53.607Z')).toStrictEqual('30/11/2021');
   });
-  it("should show if it's even or odd", () => {
-    expect(parOrImpar(2)).toStrictEqual('30/11/2021');
+  it("should show if it is even or odd, in which case it must show 'Even'", () => {
+    expect(parOrImpar(2)).toStrictEqual('Par');
+  });
+  it("should show if it is even or odd, in which case it must show 'Odd'", () => {
+    expect(parOrImpar(3)).toStrictEqual('Impar');
+  });
+  it("should sort the list , from the oldest", () => {
+    expect(mockInputSearch.sort((a: any, b: any) =>
+      sortFunction(a, b, 'Mais antigas'),
+    )).toStrictEqual(mockupSortAntigas);
+  });
+  it("should sort the list , of the youngest", () => {
+    expect(mockInputSearch.sort((a: any, b: any) =>
+      sortFunction(a, b, 'Mais novas'),
+    )).toStrictEqual(mockupSortNovas);
   });
 });
 
